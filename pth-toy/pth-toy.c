847356292a93ca5a6b24465215a7c8317fc124d9
Andrew Miller <amiller@dappervision.com>
2014-08-13 18:31:41 -0400

    Example with file locking that fails with w=1 but is okay with w=0
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- pth-toy/pth-toy.c
+++ pth-toy/pth-toy.c
@@ -28,5 +28,22 @@ int plugin_main(int argc, char* argv[], ShadowLogFunc slogf_) {
 	LOG_MESSAGE("About to do multithread work");
 	toy_pthread_doWork();
 	LOG_MESSAGE("Done");
+
+	LOG_MESSAGE("Creating file");
+	FILE *f = fopen("/tmp/pth-toy-lock","w");
+	assert(f);
+	fclose(f);
+	int hnd = open("/tmp/pth-toy-lock",O_RDWR);
+	assert(hnd != -1);
+	struct flock lock;
+	lock.l_type    = F_WRLCK;
+	lock.l_whence  = SEEK_SET;
+	lock.l_start   = 0;
+	lock.l_len     = 0;
+	int ret = fcntl(hnd, F_SETLK, &lock);
+	if (!ret) {
+	}
+	assert(ret == 0);
+
 	return 0;
 }
